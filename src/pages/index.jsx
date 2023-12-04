9b49210954b1e9862251a89d7df72b54aeef5696
AronGunnar <99695133+AronGunnar@users.noreply.github.com>
2023-12-04 16:42:01 +0100

    Added search bar to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import LayoutGlobal from '@/components/Layout/LayoutGlobal'
+import SearchBar from '@/components/utils/SearchBar'
+import { Container } from '@/components/utils/Container'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <LayoutGlobal></LayoutGlobal>
+  return (
+    <LayoutGlobal>
+      <Container>
+        <div className='flex h-[80vh] w-full flex-col items-center justify-center'>
+          <SearchBar></SearchBar>
+          <p className='text-zinc-600'>Search for products or materials.</p>
+        </div>
+      </Container>
+    </LayoutGlobal>
+  )
 }
