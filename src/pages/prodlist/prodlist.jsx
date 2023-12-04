2c025732fa7f5e359fd27085108d4cb664ec4ef4
AronGunnar <99695133+AronGunnar@users.noreply.github.com>
2023-12-04 17:42:38 +0100

    list temp design done

--- src/pages/prodlist/prodlist.jsx
+++ src/pages/prodlist/prodlist.jsx
@@ -1,53 +1,78 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import LayoutGlobal from '@/components/Layout/LayoutGlobal'
+import { Container } from '@/components/utils/Container'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <LayoutGlobal>
-      <div>
-        <h2>HTML Table</h2>
+      <Container>
+        <div className='table-container h-[80vh] w-full flex items-center justify-center flex-col'>
+          <h2 className='py-[20px] w-[80%] text-2xl'>
+            Results from <span className='text-teal-600 italic'>query</span>
+          </h2>
 
-        <table>
-          <tr className=''>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-          </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-          </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-          </tr>
-        </table>
-      </div>
+          <table>
+            <tr>
+              <th>Product</th>
+              <th>Text</th>
+              <th>Text</th>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+            <tr>
+              <td>Battery</td>
+              <td>Placeholder</td>
+              <td>Placeholder</td>
+            </tr>
+          </table>
+        </div>
+      </Container>
     </LayoutGlobal>
   )
 }
