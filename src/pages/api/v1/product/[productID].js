715949f6aa84366258a66dd70801dcc45854e4a4
themooses <wilhelm.rauston@gmail.com>
2024-02-03 20:45:37 +0100

    HOLYMAYBE?

--- src/pages/api/v1/product/[productID].js
+++ src/pages/api/v1/product/[productID].js
@@ -5,13 +5,13 @@ import Product from '@/models/Product'
 const getProductById = async (req, res) => {
   console.log('heres the req', req.query)
   try {
-    const { productId } = req.query
-    console.log('heres the productid', productId)
-    if (!ObjectId.isValid(productId)) {
+    const { productID } = req.query
+    console.log('heres the productid', productID)
+    if (!ObjectId.isValid(productID)) {
       return res.status(400).json({ message: 'Invalid ID' })
     }
 
-    const productData = await Product.findOne({ _id: new ObjectId(productId) })
+    const productData = await Product.findOne({ _id: new ObjectId(productID) })
 
     if (!productData) {
       return res.status(404).json({ message: 'Product not found' })
