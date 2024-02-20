2a5b3bccb09386880c65822447677132f07bdad0
themooses <wilhelm.rauston@gmail.com>
2024-02-20 16:10:03 +0100

    scan browser, mobileview

--- src/pages/scan.jsx
+++ src/pages/scan.jsx
@@ -1,5 +1,6 @@
 import LayoutGlobal from '@/components/Layout/LayoutGlobal'
 import { Container } from '@/components/utils/Container'
+import { BrowserView, MobileView } from 'react-device-detect'
 import dynamic from 'next/dynamic'
 
 // Example for a named export, replace `NamedComponent` with the actual export name
@@ -15,11 +16,20 @@ export default function Scanner() {
   return (
     <LayoutGlobal>
       <Container>
-        <QrScanner
-          onDecode={result => console.log(result)}
-          onError={error => console.log(error?.message)}
-          audio={false}
-        />
+        <MobileView>
+          <div className='flex flex-col items-center justify-center pt-4'>
+            <div className='w-2/5'>
+              <QrScanner
+                onDecode={result => console.log(result)}
+                onError={error => console.log(error?.message)}
+                audio={false}
+              />
+            </div>
+          </div>
+        </MobileView>
+        <BrowserView>
+          <h1>To scan a QR code please open on mobile</h1>
+        </BrowserView>
       </Container>
     </LayoutGlobal>
   )
