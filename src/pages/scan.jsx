e01ed746c12292c78165b99afc608c4e6753a683
themooses <wilhelm.rauston@gmail.com>
2024-02-20 16:21:54 +0100

    f

--- src/pages/scan.jsx
+++ src/pages/scan.jsx
@@ -1,6 +1,7 @@
 import LayoutGlobal from '@/components/Layout/LayoutGlobal'
 import { Container } from '@/components/utils/Container'
 import { BrowserView, MobileView } from 'react-device-detect'
+import { useState } from 'react'
 import dynamic from 'next/dynamic'
 
 // Example for a named export, replace `NamedComponent` with the actual export name
@@ -13,7 +14,6 @@ const QrScanner = dynamic(
 )
 
 export default function Scanner() {
-  // State to hold the decoded result
   const [decodedResult, setDecodedResult] = useState('')
 
   // Handle the decoded result
@@ -21,26 +21,33 @@ export default function Scanner() {
     console.log(result)
     setDecodedResult(result) // Update state with the decoded result
   }
-
   return (
-    <div className='flex flex-col items-center justify-center pt-4'>
-      <div className='w-full'>
-        <QrScanner
-          onDecode={handleDecode}
-          onError={error => console.log(error?.message)}
-          audio={false}
-        />
-      </div>
-      {/* Input box to display the decoded result */}
-      <div className='mt-4 w-full'>
-        <input
-          type='text'
-          className='form-input mt-1 block w-full'
-          value={decodedResult} // Display the decoded result here
-          onChange={e => setDecodedResult(e.target.value)} // Allows editing the result if needed
-          placeholder='Decoded QR code result'
-        />
-      </div>
-    </div>
+    <LayoutGlobal>
+      <Container>
+        <MobileView>
+          <div className='flex flex-col items-center justify-center pt-4'>
+            <div className='w-full'>
+              <QrScanner
+                onDecode={handleDecode}
+                onError={error => console.log(error?.message)}
+                audio={false}
+              />
+            </div>
+            <div className='mt-4 w-full'>
+              <input
+                type='text'
+                className='form-input mt-1 block w-full'
+                value={decodedResult} // Display the decoded result here
+                onChange={e => setDecodedResult(e.target.value)} // Allows editing the result if needed
+                placeholder='Decoded QR code result'
+              />
+            </div>
+          </div>
+        </MobileView>
+        <BrowserView>
+          <h1>To scan a QR code please open on mobile</h1>
+        </BrowserView>
+      </Container>
+    </LayoutGlobal>
   )
 }
