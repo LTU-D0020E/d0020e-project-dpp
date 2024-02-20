50857af8efcc384ee0f44e97d2ff5fa2f0a1f32c
themooses <wilhelm.rauston@gmail.com>
2024-02-20 15:07:58 +0100

    test

--- src/components/UI/Forms/QRScanner.jsx
+++ src/components/UI/Forms/QRScanner.jsx
@@ -1,60 +1,13 @@
 import React, { useRef, useState, useEffect } from 'react'
-import jsQR from 'jsqr-es6'
+import { QrScanner } from '@yudiel/react-qr-scanner'
 
-const QRScanner = () => {
-  const videoRef = useRef(null)
-  const canvasRef = useRef(null)
-  const [qrCode, setQrCode] = useState('')
-
-  useEffect(() => {
-    // Access the user's webcam
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: 'environment' } })
-      .then(stream => {
-        videoRef.current.srcObject = stream
-        videoRef.current.play()
-      })
-      .catch(err => console.error('Error accessing the webcam:', err))
-
-    // Scan for QR codes
-    const scanningInterval = setInterval(() => {
-      if (videoRef.current && canvasRef.current) {
-        const context = canvasRef.current.getContext('2d')
-        const width = videoRef.current.videoWidth
-        const height = videoRef.current.videoHeight
-        if (width && height) {
-          canvasRef.current.width = width
-          canvasRef.current.height = height
-          context.drawImage(videoRef.current, 0, 0, width, height)
-          const imageData = context.getImageData(0, 0, width, height)
-          const code = jsQR(imageData.data, imageData.width, imageData.height, {
-            inversionAttempts: 'dontInvert',
-          })
-          if (code) {
-            setQrCode(code.data)
-            clearInterval(scanningInterval) // Stop scanning after the first QR code is found
-          }
-        }
-      }
-    }, 100) // Scan every 100 milliseconds
-
-    return () => {
-      return () => {
-        clearInterval(scanningInterval)
-        const tracks = videoRef.current.srcObject.getTracks()
-        tracks.forEach(track => track.stop())
-      }
-    }
-  }, [])
-
+export default function QRScannerComponent() {
   return (
-    <div>
-      <video ref={videoRef} style={{ width: '100%' }}></video>
-
-      <canvas ref={canvasRef} style={{ width: '100%' }}></canvas>
-      {qrCode && <p>QR Code Detected: {qrCode}</p>}
-    </div>
+    <>
+      <QrScanner
+        onDecode={result => console.log(result)}
+        onError={error => console.log(error?.message)}
+      />
+    </>
   )
 }
-
-export default QRScanner
