ee0c79322de22d087ebf9bfa476cf6c23b14e4ee
themooses <wilhelm.rauston@gmail.com>
2024-03-04 16:14:54 +0100

    f

--- src/models/KeyDocument.js
+++ src/models/KeyDocument.js
@@ -2,18 +2,23 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const FieldKeysSchema = new Schema({
-  privateKey: { type: String, required: true },
-  publicKey: { type: String, required: true },
-})
-
 const KeyDocumentSchema = new Schema({
-  cid: { type: String, required: true, unique: true },
-  fieldType: {
-    remanufacturing_events: FieldKeysSchema,
-    shipping: FieldKeysSchema,
-    makes: FieldKeysSchema,
-    made_from: FieldKeysSchema,
+  cid: String,
+  remanufacturing_events: {
+    privatekey: String,
+    publickey: String,
+  },
+  shipping: {
+    privatekey: String,
+    publickey: String,
+  },
+  makes: {
+    privatekey: String,
+    publickey: String,
+  },
+  made_from: {
+    privatekey: String,
+    publickey: String,
   },
 })
 
